db65dc14167d04060c237cb62d93ffd9a3defa28
huanglongdejia <huanglongdejia@163.com>
2018-08-20 23:39:40 +0800

    数据提取

--- webobigdata/dataset.py
+++ webobigdata/dataset.py
@@ -1,25 +1,36 @@
 # -*- coding: utf-8 -*-
-"""搭建语料库，及分词"""
-import os
-import os.path
-import codecs
 
-filePaths = []
-fileContents = []
-for root, dirs, files in os.walk(
-        'C:\\Users\\huanglong6\\Downloads\\Weibo Data\\Weibo Data\\weibo_train_data(new)'
-):
-    for name in files:
-        filePath = os.path.join(root, name)
-        filePaths.append(filePath)
-        f = codecs.open(filePath, 'r', 'utf-8')
-        fileContent = f.read()
-        f.close()
-        fileContents.append(fileContent)
+import pandas as pd
+# 数据集对象
+class DataSet(object):
+    def __init__(self,uid,mid,time,forward_count,comment_count,like_count,content):
+        self.uid = uid
+        self.mid = mid
+        self.time = time
+        self.forward_count = forward_count
+        self.comment_count =comment_count
+        self.like_count =like_count
+        self.content =content
 
-import pandas
+# 读取数据集
+class DataSetOperator(object):
+    def __init__(self,file_path):
+        self.file_path = file_path
 
-corpos = pandas.DataFrame({
-    'filePath': filePaths,
-    'fileContent': fileContents
-})
+    def getDataset(self):
+        lines = [line for line in open(self.file_path,'r',encoding='utf-8')]
+        dataSet = []
+        column = ['uid','mid','time','forward_count','comment_count','like_count','content']
+        for line in lines:
+            p = line.strip().split('\t')
+            if line.strip() == '' or len(p) != 7:
+                continue
+            dataSet.append(p)
+
+        return pd.DataFrame(dataSet,columns=column)
+
+if __name__ == '__main__':
+    dataSetOperator = DataSetOperator('C:\\Users\huanglong\Desktop\Weibo Data\Weibo Data\weibo_train_data(new)\weibo_train_data.txt')
+    dataset = dataSetOperator.getDataset()
+    print(dataset)
+
